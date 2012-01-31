0a1c521a82c030b5f39502cb69f1ddf2a6df2cb8
Benjamin Jackson <bhjackson@gmail.com>
2012-01-31 16:52:13 -0500

    Removed monkey-patched Float#round from hackish beginning code after receiving shit from @dblock. Don't need it now anyway. Thanks, Daniel.

--- lib/muckraker.rb
+++ lib/muckraker.rb
@@ -5,13 +5,6 @@ require 'fileutils'
 require 'titlecase'
 require 'erb'
 
-class Float
-    alias_method :round_orig, :round
-    def round(n=0)
-        (self * (10.0 ** n)).round_orig * (10.0 ** (-n))
-    end
-end
-
 class String
     # normalize against differences in names (e.g. , LLC vs. just LLC)
     def normalize

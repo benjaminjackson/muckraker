7118d49f8b4fe184d0cbd4f4a9cfd0c1c13a9b14
Benjamin Jackson <bhjackson@gmail.com>
2012-02-08 00:19:05 -0500

    Added total disbursements to charts for top campaigns.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source "http://rubygems.org"
 
+gem "json", ">= 1.6.5"
 gem "campaign_cash", ">= 2.0.7"
 gem "titlecase", ">= 0.1.1"
 gem "sinatra", ">= 1.3.1"
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'sinatra'
 require "sinatra/reloader" if development?
+require 'json'
 require './lib/muckraker'
 require './models'
 require './stats'
@@ -15,19 +16,18 @@ DataMapper.auto_upgrade!
 get '/' do
 	@columns = { :names => ['Campaign', 'Amount'], :types => ['string', 'number'] }
 	@most_supported_campaigns = Campaign.most_supported
-	@most_supported_campaigns_data = @most_supported_campaigns.map do |campaign|
-		campaign.total_expenditures(:support_or_oppose => 'S')
-	end
+	@most_supported_campaigns_data = [
+		{'name' => 'Total Expenditures', 'data' => @most_supported_campaigns.map { |campaign| campaign.total_expenditures(:support_or_oppose => 'S') } },
+		{'name' => 'Total Disbursements', 'data' => @most_supported_campaigns.map { |campaign| campaign.total_disbursements } }
+	]
 	@most_supported_campaigns_disbursements_data = @most_supported_campaigns.map do |campaign|
 		campaign.total_disbursements
 	end
 	@most_opposed_campaigns = Campaign.most_opposed
-	@most_opposed_campaigns_data = @most_opposed_campaigns.map do |campaign|
-		campaign.total_expenditures(:support_or_oppose => 'O')
-	end
-	@most_opposed_campaigns_disbursements_data = @most_opposed_campaigns.map do |campaign|
-		campaign.total_disbursements
-	end
+	@most_opposed_campaigns_data = [
+		{'name' => 'Total Expenditures', 'data' => @most_opposed_campaigns.map { |campaign| campaign.total_expenditures(:support_or_oppose => 'O') } },
+		{'name' => 'Total Disbursements', 'data' => @most_opposed_campaigns.map { |campaign| campaign.total_disbursements } }
+	]
   	erb :index
 end
 

7307809f4b594aedc85c5eb911f1994f04bae26a
Benjamin Jackson <bhjackson@gmail.com>
2012-02-08 13:02:37 -0500

    Cleaned up unneeded code.

--- app.rb
+++ app.rb
@@ -14,15 +14,11 @@ DataMapper.finalize
 DataMapper.auto_upgrade!
 
 get '/' do
-	@columns = { :names => ['Campaign', 'Amount'], :types => ['string', 'number'] }
 	@most_supported_campaigns = Campaign.most_supported
 	@most_supported_campaigns_data = [
 		{'name' => 'Total Expenditures', 'data' => @most_supported_campaigns.map { |campaign| campaign.total_expenditures(:support_or_oppose => 'S') } },
 		{'name' => 'Total Disbursements of Campaign', 'data' => @most_supported_campaigns.map { |campaign| campaign.total_disbursements } }
 	]
-	@most_supported_campaigns_disbursements_data = @most_supported_campaigns.map do |campaign|
-		campaign.total_disbursements
-	end
 	@most_opposed_campaigns = Campaign.most_opposed
 	@most_opposed_campaigns_data = [
 		{'name' => 'Total Expenditures', 'data' => @most_opposed_campaigns.map { |campaign| campaign.total_expenditures(:support_or_oppose => 'O') } },

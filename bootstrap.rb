2cca7f92499e305376fc15693fce2a5f5db7d40a
Benjamin Jackson <bhjackson@gmail.com>
2012-02-08 12:57:49 -0500

    Added SuperPACs to bootstrap.rb.

--- bootstrap.rb
+++ bootstrap.rb
@@ -5,7 +5,8 @@ require './models'
 class Muckraker::Bootstrap
 	def self.load
         CampaignCash::Base.api_key = ENV['API_KEY']
-		load_candidates
+        load_candidates
+        load_superpacs
 		load_expenditures
 	end
 
@@ -24,29 +25,53 @@ class Muckraker::Bootstrap
         	attrs = Hash[props.map { |name|	[name, full_candidate.send(name)] }]
         	attrs['id'] = nil
         	attrs['remote_id'] = full_candidate.id
-        	Campaign.first_or_create({:remote_id => full_candidate.id}, attrs)
+        	campaign = Campaign.first_or_create({:remote_id => full_candidate.id}, attrs)
+            load_committee_for_candidate(candidate, campaign)
         end
 	end
+
+    def self.load_committee_for_candidate candidate, campaign
+        if candidate.committee_id
+            puts "Loading committee for candidate #{candidate.name}, #{candidate.office}..."
+            committee = CampaignCash::Committee.find(candidate.committee_id)
+            create_committee(committee, campaign)
+        end
+    end
+
+    def self.create_committee committee, campaign=nil
+        c = CampaignCash::Committee.new
+        props = Committee.properties.to_ary.map { |p| p.name }.select { |p| c.respond_to? p }
+        attrs = Hash[props.map { |name| [name, committee.send(name)] }]
+        attrs['id'] = nil
+        attrs['remote_id'] = committee.id
+        attrs['campaign'] = campaign
+        attrs.keys.each { |key| attrs[key] = attrs[key].to_f if key.to_s.include?('total_') }
+        Committee.first_or_create({:remote_id => committee.id}, attrs)
+    end
+
+    def self.load_superpacs
+        CampaignCash::Committee.superpacs.each do |committee|
+            create_committee(CampaignCash::Committee.find(committee.id))
+        end
+    end
 
 	def self.load_expenditures
 		e = CampaignCash::IndependentExpenditure.new
        	props = IndependentExpenditure.properties.to_ary.map { |p| p.name }.select { |p| e.respond_to? p }
        	total_expenditures = 0
-        Campaign.all.each_with_index do |campaign, index|
+        Campaign.all.to_a[3478..-1].each_with_index do |campaign, index|
             puts "Loading expenditures for candidate #{index + 1} of #{Campaign.count}: #{campaign.name}, #{campaign.office}..."
             expenditures = CampaignCash::IndependentExpenditure.candidate(campaign.remote_id, 2012)
-            puts expenditures.length
             total_expenditures += expenditures.length
             expenditures.each do |exp|
         		attrs = Hash[props.map { |name| [name, exp.send(name)] }]
         		attrs[:amount] = attrs[:amount].to_f * 10
         		attrs[:campaign] = campaign
+                attrs.keys.each { |key| attrs[key] = attrs[key].to_f if key.to_s.include?('total_') }
             	independent_exp = IndependentExpenditure.first_or_create({:transaction_id => attrs[:transaction_id], :date_received => attrs[:date_received]}, attrs)
             end
         end
-        puts "total_expenditures"
-        puts total_expenditures
-        puts "count:"
+        puts "Total Expenditures:"
         puts IndependentExpenditure.count
 	end
 end
